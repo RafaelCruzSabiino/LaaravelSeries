31c122a19f681ed2a058b4bbd3cf5df216c4bb22
Rafael Sabino <rcsabino@paschoalotto.com.br>
2024-06-25 16:30:46 -0300

    feat: Editar

--- routes/web.php
+++ routes/web.php
@@ -7,12 +7,4 @@ Route::get('/', function () {
     return redirect('/series');
 });
 
-Route::resource('/series', SeriesController::class)->only(['index', 'create', 'store', 'destroy']);
-
-// Route::delete('/series/destroy/{id}', [SeriesController::class, 'destroy'])->name('series.destroy');
-
-// Route::controller(SeriesController::class)->group(function(){
-//     Route::get('/series', 'index')->name('series.index');
-//     Route::get('/series/criar', 'create')->name('series.create');
-//     Route::post('/series/salvar', 'store')->name('series.store');
-// });
+Route::resource('/series', SeriesController::class)->except(['show']);
