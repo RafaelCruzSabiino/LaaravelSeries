82ea88da6c6cdbba40ef70f3c5eb20082fd06a09
Rafael Sabino <rcsabino@paschoalotto.com.br>
2024-07-03 13:24:11 -0300

    feat: Inicio transaction

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -7,6 +7,7 @@ use App\Models\Season;
 use App\Models\Episode;
 use App\Http\Requests\SeriesFormRequest;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class SeriesController extends Controller
 {
@@ -26,32 +27,35 @@ class SeriesController extends Controller
 
     public function store(SeriesFormRequest $request)
     {
-        $serie    = Serie::create($request->all());
-        $seasons  = [];
-        $episodes = [];
-
-        for($i = 1; $i <= $request->seasonsQty; $i++)
-        {
-            $seasons[] = [
-                'series_id' => $serie->id,
-                'number' => $i
-            ];
-        }
-
-        Season::insert($seasons);
-
-        foreach($serie->seasons as $season)
-        {
-            for($j = 1; $j <= $request->seasonsQty; $j++)
+        $serie = null;
+        DB::transaction(function () use ($request, &$serie){
+            $serie    = Serie::create($request->all());
+            $seasons  = [];
+            $episodes = [];
+    
+            for($i = 1; $i <= $request->seasonsQty; $i++)
             {
-                $episodes[] = [
-                    'season_id' => $season->id,
-                    'number' => $j
+                $seasons[] = [
+                    'series_id' => $serie->id,
+                    'number' => $i
                 ];
             }
-        }
-        
-        Episode::insert($episodes);
+    
+            Season::insert($seasons);
+    
+            foreach($serie->seasons as $season)
+            {
+                for($j = 1; $j <= $request->epsiodeQty; $j++)
+                {
+                    $episodes[] = [
+                        'season_id' => $season->id,
+                        'number' => $j
+                    ];
+                }
+            }
+            
+            Episode::insert($episodes);
+        });
 
         return to_route('series.index')->with('mensagem.sucesso', "Série '{$serie->nome}' adicionada com sucesso");
     }
@@ -63,7 +67,7 @@ class SeriesController extends Controller
         return to_route('series.index')->with('mensagem.sucesso', "Série '{$series->nome}' removida com sucesso");
     }
 
-    public function edit(Serie $series)
+    public function eidt(Serie $series)
     {
         return view('series.edit')->with('serie', $series);
     }
