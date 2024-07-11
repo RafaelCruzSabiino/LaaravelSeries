a7dba136e6d54e37ea3444b78ba8d20d44930ea0
Rafael Sabino <rcsabino@paschoalotto.com.br>
2024-07-11 15:46:44 -0300

    feat: Botoes corretos

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -39,7 +39,7 @@ class SeriesController extends Controller
         return to_route('series.index')->with('mensagem.sucesso', "Série '{$series->nome}' removida com sucesso");
     }
 
-    public function eidt(Serie $series)
+    public function edit(Serie $series)
     {
         return view('series.edit')->with('serie', $series);
     }
